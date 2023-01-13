1720e0b239af41656b1b3da48b09d9de46bfb60c
Tendsin Mende <tendsin@protonmail.com>
2023-01-13 20:45:10 +0100

    change to eddyb's version

--- crates/shader/src/lib.rs
+++ crates/shader/src/lib.rs
@@ -17,40 +17,62 @@ use spirv_std::{spirv, RuntimeArray};
 use spirv_std::num_traits::Float;
 
 #[spirv(typed_buffer)]
-pub struct TypedBuffer<T: 'static>{
-    //borrowing the hidden data trick
-    data: u32,
-    dataty: PhantomData<T>
+pub struct TypedBuffer<T: ?Sized> {
+    // spooky! this field does not exist, so if it's referenced in rust code, things will explode
+    _do_not_touch: u32,
+    _phantom: PhantomData<T>,
 }
 
-
-impl<T: 'static> Deref for TypedBuffer<T>{
+impl<T> Deref for TypedBuffer<T> {
     type Target = T;
     #[gpu_only]
-    fn deref(&self) -> &Self::Target {
-        unsafe{
+    fn deref(&self) -> &T {
+        unsafe {
             core::arch::asm! {
                 "%uint = OpTypeInt 32 0",
                 "%uint_0 = OpConstant %uint 0",
-                "%result = OpAccessChain _ {arr} %uint_0",
-                "OpReturnValue %result",
-                arr = in(reg) self,
+                "%inner_ptr = OpAccessChain _ {buffer} %uint_0",
+                "OpReturnValue %inner_ptr",
+                buffer = in(reg) self,
                 options(noreturn),
             }
         }
     }
 }
 
-impl<T: 'static> DerefMut for TypedBuffer<T>{
+impl<T> Deref for TypedBuffer<[T]> {
+    type Target = [T];
     #[gpu_only]
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        unsafe{
+    fn deref(&self) -> &[T] {
+        unsafe {
             core::arch::asm! {
                 "%uint = OpTypeInt 32 0",
                 "%uint_0 = OpConstant %uint 0",
-                "%result = OpAccessChain _ {arr} %uint_0",
-                "OpReturnValue %result",
-                arr = in(reg) self,
+                "%inner_ptr = OpAccessChain _ {buffer} %uint_0",
+                "%inner_len = OpArrayLength %uint {buffer} 0",
+                "%inner_slice_ptr = OpCompositeConstruct typeof*{dummy_ref_to_slice_ref} %inner_ptr %inner_len",
+                "OpReturnValue %inner_slice_ptr",
+                buffer = in(reg) self,
+                dummy_ref_to_slice_ref = in(reg) &(&[] as &[T]),
+                options(noreturn),
+            }
+        }
+    }
+}
+
+impl<T> DerefMut for TypedBuffer<[T]> {
+    #[gpu_only]
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        unsafe {
+            core::arch::asm! {
+                "%uint = OpTypeInt 32 0",
+                "%uint_0 = OpConstant %uint 0",
+                "%inner_ptr = OpAccessChain _ {buffer} %uint_0",
+                "%inner_len = OpArrayLength %uint {buffer} 0",
+                "%inner_slice_ptr = OpCompositeConstruct typeof*{dummy_ref_to_slice_ref} %inner_ptr %inner_len",
+                "OpReturnValue %inner_slice_ptr",
+                buffer = in(reg) self,
+                dummy_ref_to_slice_ref = in(reg) &(&[] as &[T]),
                 options(noreturn),
             }
         }
@@ -61,21 +83,22 @@ impl<T: 'static> DerefMut for TypedBuffer<T>{
 pub fn main(
     #[spirv(push_constant)] push: &Push,
     #[spirv(global_invocation_id)] id: UVec3,
-    #[spirv(descriptor_set = 0, binding = 0, storage_buffer)] buffers_a: &RuntimeArray<TypedBuffer<RuntimeArray<u32>>>,
-    #[spirv(descriptor_set = 0, binding = 0, storage_buffer)] buffers_b: &mut RuntimeArray<TypedBuffer<RuntimeArray<u32>>>
+    #[spirv(descriptor_set = 0, binding = 0, storage_buffer)] buffers_a: &RuntimeArray<TypedBuffer<[u32]>>,
+    #[spirv(descriptor_set = 0, binding = 0, storage_buffer)] buffers_b: &mut RuntimeArray<TypedBuffer<[u32]>>
 ) {
     let widx = id.x;
     if widx > push.size{
         return;
     }
 
-    let a = unsafe{
-        buffers_a.index(push.src_hdl.index() as usize).deref().index(widx as usize)
+    //load
+    let mut a = unsafe{
+        buffers_a.index(push.src_hdl.index() as usize)[widx as usize]
     };
 
     //store
     unsafe{
-        *buffers_b.index_mut(push.dst_hdl.index() as usize).deref_mut().index_mut(widx as usize) = *a;
+        buffers_b.index_mut(push.dst_hdl.index() as usize)[widx as usize] = a;
     }
 
 }
